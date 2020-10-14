9aceda6dea3d274ead13ffa7c61c79e37215c528
Fuad Agil <far1023@users.noreply.github.com>
2020-10-14 14:57:45 +0700

    reset assets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import '../assets/style.css';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
-import { AnimatedSwitch } from 'react-router-transition';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import posed, { PoseGroup } from 'react-pose';
+
 
 import About from "./About";
 import Tech from "./Tech";
 
+const RouteContainer = posed.div({
+	enter: { opacity: 1, delay: 300, beforeChildren: false },
+	exit: { opacity: 0 }
+});
+
 function App() {
 	return (
 		<Router>
-		<AnimatedSwitch atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }} className="switch-wrapper">
-		<div>
-		<Switch>
-		<Route exact path="/" component={About} >
-		</Route>
-		<Route path="/about" component={About} />
-		<Route path="/tech" component={Tech} />
-		</Switch>
-		<div className="link" id="route">
-		<Link to="/about" className="route">About</Link> • <Link to="/tech" className="route">Tech</Link>
-		</div>
-		</div>
-		</AnimatedSwitch>
+			<div>
+				<Route render={({ location }) => (
+					<PoseGroup>
+						<RouteContainer key={location.pathname}>	
+							<Switch>
+								<Route exact path="/" component={About} key="" />
+								<Route path="/about" component={About} key="about" />
+								<Route path="/tech" component={Tech} key="tech" />
+							</Switch>
+						</RouteContainer>
+					</PoseGroup>
+				)}/>
+				<div className="link" id="route">
+					<Link to="/about" className="route">About</Link> • <Link to="/tech" className="route">Tech</Link>
+				</div>
+			</div>
 		</Router>
-		);
+	);
 }
 
 export default App;
