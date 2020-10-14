314c0f3568b6791d0dc6cf5f56a3b0e23dec083e
Fuad Agil <far1023@users.noreply.github.com>
2020-10-14 16:30:25 +0700

    add Tech

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
-import '../assets/style.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import posed, { PoseGroup } from 'react-pose';
-
-
 import About from "./About";
 import Tech from "./Tech";
 
+import '../assets/style.css';
+
 const RouteContainer = posed.div({
-	enter: { opacity: 1, delay: 300, beforeChildren: false },
-	exit: { opacity: 0 }
+	exit: { opacity: 0 },
+	enter: { opacity: 1, delay: 300, beforeChildren: true },
 });
+
 
 function App() {
 	return (
 		<Router>
-			<div>
-				<Route render={({ location }) => (
-					<PoseGroup>
-						<RouteContainer key={location.pathname}>	
-							<Switch>
-								<Route exact path="/" component={About} key="" />
-								<Route path="/about" component={About} key="about" />
-								<Route path="/tech" component={Tech} key="tech" />
-							</Switch>
-						</RouteContainer>
-					</PoseGroup>
-				)}/>
-				<div className="link" id="route">
-					<Link to="/about" className="route">About</Link> • <Link to="/tech" className="route">Tech</Link>
+			<Route render={({ location }) => (
+				<div id="site-container">
+					<div id="content-container">
+						<PoseGroup>
+							<RouteContainer key={location.key}>
+								<Switch location={location}>
+									<Route exact path="/" component={About} key="" />
+									<Route path="/about" component={About} key="about" />
+									<Route path="/tech" component={Tech} key="tech" />
+								</Switch>
+							</RouteContainer>
+						</PoseGroup>
+					</div>
+					<div className="link" id="route">
+						<Link to="/about" className="route">About</Link> • <Link to="/tech" className="route">Tech</Link>
+					</div>
 				</div>
-			</div>
+			)} />
 		</Router>
 	);
 }
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,9 +4,16 @@ import '../assets/style.css';
 function Tech() {
 	return (
 		<div>
-		<div className="tech"><i className="fab fa-php fa-4x fa-fw" /><i className="fab fa-js fa-4x fa-fw" /><i className="fab fa-bootstrap fa-4x fa-fw" /></div>
+			<div className="tech">
+				<i className="fab fa-php fa-4x fa-fw" title="PHP" />
+				<i className="fab fa-js-square fa-4x fa-fw" title="Javascript" />
+				<i className="fab fa-bootstrap fa-4x fa-fw" title="Bootstrap" />
+				<i className="fab fa-node-js fa-4x fa-fw" title="Node.js" />
+				<i className="fab fa-react fa-4x fa-fw" title="React" />
+				<i className="fab fa-git-alt fa-4x fa-fw" title="Git" />
+			</div>
 		</div>
-		);
+	);
 }
 
 export default Tech;
