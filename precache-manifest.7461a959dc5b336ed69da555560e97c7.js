99977f59f15fdcf8ba4384d7d8e20d4f6b77cac7
Fuad Agil <fuadagilr@gmail.com>
2021-12-09 18:33:55 +0700

    Updates

---
+++ precache-manifest.7461a959dc5b336ed69da555560e97c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957580458495315c65e1d19617add496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2931febb349b3caaf2a7",
+    "url": "/static/css/main.e9e2161b.chunk.css"
+  },
+  {
+    "revision": "5deb76adb0dd68d10ed7",
+    "url": "/static/js/2.15577a5a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.15577a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2931febb349b3caaf2a7",
+    "url": "/static/js/main.7dc5a12e.chunk.js"
+  },
+  {
+    "revision": "aa7c1ffb132bd348da10",
+    "url": "/static/js/runtime-main.baf67b5f.js"
+  },
+  {
+    "revision": "e91b2d3aa0963bc7d06b393558a626ff",
+    "url": "/static/media/corona.e91b2d3a.webp"
+  },
+  {
+    "revision": "62aa96842a7b6d2c63c35520d11b6c20",
+    "url": "/static/media/epusda.62aa9684.webp"
+  },
+  {
+    "revision": "574eb2ccca814ecc91c3bb6e186b338e",
+    "url": "/static/media/silau.574eb2cc.webp"
+  },
+  {
+    "revision": "0260b3340f2fe5bbd8173e9d47a777ec",
+    "url": "/static/media/sipasti.0260b334.webp"
+  }
+]);
